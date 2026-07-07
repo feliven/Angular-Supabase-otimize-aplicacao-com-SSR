c4deb63179d0ad397f002ec4e26abcfa03a22c8f
FVS-MBA <feliven@gmail.com>
2026-07-07 20:28:55 -0300

    implementa Supabase

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, type OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
 import { Footer } from './shared/components/footer/footer';
+import { SupabaseService } from './shared/services/supabase.service';
+import type { Product } from './shared/types/types';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,22 @@ import { Footer } from './shared/components/footer/footer';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App implements OnInit {
+  supabaseService = inject(SupabaseService);
+
+  products: Product[] = [];
+
+  ngOnInit() {
+    this.loadProducts();
+  }
+
+  async loadProducts() {
+    const { data, error } = await this.supabaseService.getProducts();
+    if (error) {
+      console.error('Error fetching products:', error);
+    } else {
+      this.products = data;
+      console.table(this.products);
+    }
+  }
+}
---
+++ src/app/shared/services/supabase.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SupabaseService {
+  private supabase: SupabaseClient;
+
+  constructor() {
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+  }
+
+  getProducts() {
+    return this.supabase.from('products').select('*');
+  }
+}
