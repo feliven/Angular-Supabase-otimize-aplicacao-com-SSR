a2947bf5d2381d5c1627faded597d9b5b0ffc0c4
FVS-Thiago <feliven@gmail.com>
2026-07-13 18:53:37 -0300

    simplifica sintaxe do teste da diretiva

--- src/app/directives/shell-render.directive.spec.ts
+++ src/app/directives/shell-render.directive.spec.ts
@@ -3,65 +3,39 @@ import { TestBed } from '@angular/core/testing';
 import { ShellRenderDirective } from './shell-render.directive';
 
 describe('ShellRenderDirective', () => {
-  it('should create an instance', () => {
-    const mockTemplateRef = {} as TemplateRef<any>;
-    const mockViewContainerRef = {} as ViewContainerRef;
+  const mockTemplateRef = {} as TemplateRef<unknown>;
+  let mockViewContainerRef: {
+    createEmbeddedView: ReturnType<typeof vi.fn>;
+    clear: ReturnType<typeof vi.fn>;
+  };
 
+  function createDirective(platformId: string): ShellRenderDirective {
     TestBed.configureTestingModule({
       providers: [
-        ShellRenderDirective,
-        { provide: PLATFORM_ID, useValue: 'browser' },
+        { provide: PLATFORM_ID, useValue: platformId },
         { provide: TemplateRef, useValue: mockTemplateRef },
         { provide: ViewContainerRef, useValue: mockViewContainerRef },
       ],
     });
+    return TestBed.runInInjectionContext(() => new ShellRenderDirective());
+  }
 
-    const directive = TestBed.runInInjectionContext(() => new ShellRenderDirective());
-    expect(directive).toBeTruthy();
+  beforeEach(() => {
+    mockViewContainerRef = { createEmbeddedView: vi.fn(), clear: vi.fn() };
+  });
+
+  it('should create an instance', () => {
+    expect(createDirective('browser')).toBeTruthy();
   });
 
   it('should render template on server', () => {
-    const mockTemplateRef = {} as TemplateRef<any>;
-    const mockViewContainerRef = {
-      createEmbeddedView: vi.fn(),
-      clear: vi.fn(),
-    } as unknown as ViewContainerRef;
-
-    TestBed.configureTestingModule({
-      providers: [
-        ShellRenderDirective,
-        { provide: PLATFORM_ID, useValue: 'server' },
-        { provide: TemplateRef, useValue: mockTemplateRef },
-        { provide: ViewContainerRef, useValue: mockViewContainerRef },
-      ],
-    });
-
-    const directive = TestBed.runInInjectionContext(() => new ShellRenderDirective());
-    directive.ngOnInit();
-
+    createDirective('server').ngOnInit();
     expect(mockViewContainerRef.createEmbeddedView).toHaveBeenCalledWith(mockTemplateRef);
     expect(mockViewContainerRef.clear).not.toHaveBeenCalled();
   });
 
   it('should clear container on browser', () => {
-    const mockTemplateRef = {} as TemplateRef<any>;
-    const mockViewContainerRef = {
-      createEmbeddedView: vi.fn(),
-      clear: vi.fn(),
-    } as unknown as ViewContainerRef;
-
-    TestBed.configureTestingModule({
-      providers: [
-        ShellRenderDirective,
-        { provide: PLATFORM_ID, useValue: 'browser' },
-        { provide: TemplateRef, useValue: mockTemplateRef },
-        { provide: ViewContainerRef, useValue: mockViewContainerRef },
-      ],
-    });
-
-    const directive = TestBed.runInInjectionContext(() => new ShellRenderDirective());
-    directive.ngOnInit();
-
+    createDirective('browser').ngOnInit();
     expect(mockViewContainerRef.clear).toHaveBeenCalled();
     expect(mockViewContainerRef.createEmbeddedView).not.toHaveBeenCalled();
   });
